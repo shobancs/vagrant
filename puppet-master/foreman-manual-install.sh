65ee90c20ef453f6ff770bea132348fbdf8e9c73
Shoban Cheekuru <scheekur@cisco.com>
2019-11-14 05:05:54 -0500

    new changes for foreman setup

--- puppet-master/foreman-manual-install.sh
+++ puppet-master/foreman-manual-install.sh
@@ -1,25 +1,20 @@
-sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-sudo rpm -ivh http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum -y install http://yum.theforeman.org/releases/1.9/el7/x86_64/foreman-release.rpm
-sudo yum update -y
-sudo yum -y install foreman-installer
-sestatus
-#disable SElinux, replace SELINUX=disabled
 
-vi /etc/sysconfig/selinux
-sestatus
-
-sudo foreman-installer
-
-
-#Agent setup
+#setup Puppet master and Foreman servers
 
 sudo yum -y install ntp ntpdate
 
 sudo systemctl start ntpd
 sudo systemctl enable ntpd#
 #Disable SElinux
-vim /etc/sysconfig/selinux
+sudo vi /etc/sysconfig/selinux
+SELINUX=disabled
+
+#edit the DNS configuration
+sudo /etc/hosts
+127.0.0.1  foreman.cheekuru.com foreman localhost
+#127.0.1.1 foreman.cheekuru.com foreman
+172.16.40.11 foreman.cheekuru.com puppet
+172.16.40.12 node-1.cheekuru.com node-1
 
 #Latest version of puppet lab setup
 sudo yum -y install https://yum.puppet.com/puppet6-release-el-7.noarch.rpm
@@ -30,3 +25,27 @@ sudo yum -y install foreman-installer
 #edit /etc/host with wrong dns entries
 
 sudo foreman-installer
+sudo su -
+puppet agent -t
+puppet module install puppetlabs/stdlib
+
+#foreman password reset
+
+foreman-rake permissions:reset
+
+
+#Install puppet agents on the nodes
+
+sudo yum -y install https://yum.puppet.com/puppet6-release-el-7.noarch.rpm
+sudo yum -y install http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum update -y
+
+sudo yum install -y puppet-agent
+#copy puppet agent configuration from the master
+vi /etc/puppetlabs/puppet/puppet.conf
+# Clean puppet agent certs
+ rm -rf /etc/puppetlabs/puppet/ssl/
+
+ puppetserver ca generate --certname <CERTNAME> --subject-alt-names <DNS ALT NAMES> --ca-client
+
+puppet agent -t
